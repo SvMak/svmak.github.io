da16798226d884f5e60f5b1f8de8b34e65b7a4eb
Sviatoslav Makhynko <sviat.mak@gmail.com>
2024-06-30 16:23:27 +0200

    change: title

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: "My Site",
+  title: "My Doc",
   tagline: "Dinosaurs are cool",
   favicon: "img/favicon.ico",
 
